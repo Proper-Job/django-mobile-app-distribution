524a7c64a898b9d0a939a7f4c3b1b3509376c31d
Moritz Pfeiffer <moritz.pfeiffer@alp-phone.ch>
2014-11-19 15:44:23 +0100

    Added fake .plist extension to plist url.

--- django_mobile_app_distribution/urls.py
+++ django_mobile_app_distribution/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'django_mobile_app_distribution.views.index', name='django_mobile_app_distribution_index'),
     url(r'^apk/(?P<app_id>\d{1,10})$', 'django_mobile_app_distribution.views.send_apk', name='django_mobile_app_distribution_send_apk'),
-    url(r'^plist/(?P<app_id>\d{1,10})$', 'django_mobile_app_distribution.views.ios_app_plist', name='django_mobile_app_distribution_ios_app_plist'),
+    url(r'^plist/(?P<app_id>\d{1,10})\.plist$', 'django_mobile_app_distribution.views.ios_app_plist', name='django_mobile_app_distribution_ios_app_plist'),
 )
